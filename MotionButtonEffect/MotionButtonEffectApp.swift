02e803c1900ce69fcd13a947b41594484d44982e
Raul <rauldronk@gmail.com>
2023-04-13 10:16:28 +0300

    Initial Commit

---
+++ MotionButtonEffect/MotionButtonEffectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MotionButtonEffectApp.swift
+//  MotionButtonEffect
+//
+//  Created by Raul on 13.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct MotionButtonEffectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
